3a9374277bae88341db940f6368b5f2292157889
TranChiKha030905 <enhokcon@gmail.com>
2025-03-17 23:34:04 +0700

    noi day file chinh thuc

---
+++ src/main/java/com/example/childgrowthsystem_01/ChildGrowthSystem01Application.java
@@ -0,0 +1,13 @@
+package com.example.childgrowthsystem_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildGrowthSystem01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildGrowthSystem01Application.class, args);
+    }
+
+}
